f6bf3b46f55e85d1aa8173f330b7c657255054d1
Alexis <58951139+Alex-AZ@users.noreply.github.com>
2022-02-10 16:54:31 +0100

    Add solution Todo

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -3,8 +3,8 @@ import '../styles/PlantItem.css';
 
 function PlantItem({ id, cover, name, water, light }) {
     return(
-        <li key={id} className='img-palnt-item'>
-            <img className="lmj-palnt-item-cover" src={cover} alt={`${name} cover`} />
+        <li key={id} className='lmj-plant-item'>
+            <img className="lmj-plant-item-cover" src={cover} alt={`${name} cover`} />
             {name}
             <div>
                 <CareScale careType='water' scaleValue={water} />
