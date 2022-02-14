a38793236fb9779f19a4459d0fb9357013fe4a24
Alexis <58951139+Alex-AZ@users.noreply.github.com>
2022-02-14 16:00:44 +0100

    Add P3C2-Solution with button 'add'

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,11 @@ import Cart from "./Cart";
 import ShoppingList from "./ShoppingList";
 import logo from '../assets/logo.png'
 import Footer from "./Footer";
+import { useState } from "react";
+import '../styles/Layout.css'
 
 function App() {
+  const [cart, updateCart] = useState([])
   return (
     <div>
       <Banner>
@@ -12,8 +15,8 @@ function App() {
         <h1 className='lmj-title'>La maison jungle</h1>
       </Banner>
       <div className="lmj-layout-inner">
-        <Cart />
-        <ShoppingList />
+        <Cart cart={cart} updateCart={updateCart} />
+        <ShoppingList cart={cart} updateCart={updateCart} />
       </div>
       <Footer />
     </div>
