f1fb740c83b32324c7b079b0e85c64b9687ad41a
Alexis <58951139+Alex-AZ@users.noreply.github.com>
2022-02-14 11:22:36 +0100

    Add P3C1-Solution. Todo add button 'add' below PlantItem

--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,10 @@ function App() {
         <img src={logo} alt='La maison jungle' className='lmj-logo' />
         <h1 className='lmj-title'>La maison jungle</h1>
       </Banner>
-      <Cart />
-      <ShoppingList />
+      <div className="lmj-layout-inner">
+        <Cart />
+        <ShoppingList />
+      </div>
       <Footer />
     </div>
   );
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,18 +1,34 @@
 import '../styles/Cart.css'
+import { useState } from 'react'
 
 function Cart() {
 	const monsteraPrice = 8
-	const ivyPrice = 10
-	const flowerPrice = 15
-	return (
+	const [cart, updateCart] = useState(0) // Variable d'état
+	const [isOpen, setIsOpen] = useState(false) // Variable d'état
+
+	return isOpen ? (
 		<div className="lmj-cart">
+			<button className='lmj-cart-toggle-button' 
+				onClick={() => setIsOpen(false)}>
+					Fermer
+			</button>
 			<h2>Panier</h2>
 			<ul>
-				<li>Monstera : {monsteraPrice}€</li>
-				<li>Lierre : {ivyPrice}€</li>
-				<li>Fleurs : {flowerPrice}€</li>
+				Monstera : {monsteraPrice}€
+				<button onClick={() => updateCart(cart + 1)}>
+					Ajouter
+				</button>
 			</ul>
-			Total : {monsteraPrice + ivyPrice + flowerPrice}€
+			<h3>Total : {monsteraPrice * cart}€</h3>
+			<button onClick={() => updateCart(0)}>Vider le panier</button> 
+		</div>
+	) : (
+		<div className='lmj-cart-closed'>
+			<button 
+				className='lmj-cart-toggle-button' 
+				onClick={() => setIsOpen(true)}>
+					Ouvrir le Panier
+			</button>
 		</div>
 	)
 }
